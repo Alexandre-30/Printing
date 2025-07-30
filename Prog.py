117116149e608a929a79fcb4c65ffb491e34dd09
Alexandre-30 <alexandre.ekabouma@student.rusinga.ac.ke>
2025-07-30 18:48:20 +0300

    Add files via upload

---
+++ Prog.py
@@ -0,0 +1,9 @@
+import keyword
+print("Hello World!!!")
+print("\n")
+print("My name is Alex")
+print("\n") 
+print("The following are all the python keywords")
+print(keyword.kwlist)
+print("\n")
+print("I hope theese will be usefuul")
